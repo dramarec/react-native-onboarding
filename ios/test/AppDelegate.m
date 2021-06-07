ce6af5de6fd4d7d5de0423bbfc6904e3c9f3af6b
Oleg Dramaretskyi <dramarec@gmail.com>
2021-06-07 20:38:33 +0300

    added firebase logics

--- ios/test/AppDelegate.m
+++ ios/test/AppDelegate.m
@@ -1,5 +1,7 @@
 #import "AppDelegate.h"
 
+  //firbase
+#import <Firebase.h>
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
@@ -11,6 +13,7 @@
 #import <FlipperKitNetworkPlugin/FlipperKitNetworkPlugin.h>
 #import <SKIOSNetworkPlugin/SKIOSNetworkAdapter.h>
 #import <FlipperKitReactPlugin/FlipperKitReactPlugin.h>
+
 
 static void InitializeFlipper(UIApplication *application) {
   FlipperClient *client = [FlipperClient sharedClient];
@@ -27,6 +30,11 @@ static void InitializeFlipper(UIApplication *application) {
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  //firbase
+  if ([FIRApp defaultApp] == nil) {
+    [FIRApp configure];
+  }
+
 #ifdef FB_SONARKIT_ENABLED
   InitializeFlipper(application);
 #endif
